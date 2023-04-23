ef9d3c54ad289adaf3308d767da7974f899da3c7
christian.sonic <yoh.chrisitan@gmail.com>
2023-04-23 14:29:14 -0400

    feature/characters/cacheImage
    add cache for image load from server

--- RickAndMorty_UIKit/Managers/ImageLoader.swift
+++ RickAndMorty_UIKit/Managers/ImageLoader.swift
@@ -9,17 +9,32 @@ import Foundation
 
 final class ImageLoader {
     
+    //MARK: Singleton
     static let shared = ImageLoader()
-    
     private init() { }
     
-    func downloadImage(_ url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+    //MARK: add functionality
+    private var imageDataCache = NSCache<NSString, NSData>()
+    
+    /// Get image content with URL
+    /// - Parameters:
+    ///   - url: source url
+    ///   - completion: Callback
+    public func downloadImage(_ url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        
+        let key = url.absoluteString as NSString
+        if let data = imageDataCache.object(forKey: key) {
+            completion(.success(data as Data))
+            return
+        }
         let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
             guard let data, error == nil else {
                 completion(.failure(error ?? URLError(.badServerResponse)))
                 return
             }
+            let value = data as NSData
+            self?.imageDataCache.setObject(value, forKey: key)
             completion(.success(data))
         }
         task.resume()
