6243c176b5db732eea664064462d0e84f06033d9
christian.sonic <yoh.chrisitan@gmail.com>
2023-04-22 21:22:03 -0400

    feature/api/request
    create API request

--- RickAndMorty_UIKit/APIClient/RMRequest.swift
+++ RickAndMorty_UIKit/APIClient/RMRequest.swift
@@ -10,4 +10,56 @@ import Foundation
 /// Object that represent a single API
 final class RMRequest {
     
+    /// API constants
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
+    
+    /// Desired endpoint
+    private let endpoint: RMEndpoint
+    /// path component for API, if any
+    private let pathComponents: Set<String>
+    ///query arguments for API, if any
+    private let queryParameters: [URLQueryItem]
+    /// Constructed url for the api request in string format
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            string += "/"
+            pathComponents.forEach { path in
+                string += "/\(path)"
+            }
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            let argumentString = queryParameters.compactMap ({ paramenter in
+                guard let value = paramenter.value else { return nil }
+                return "\(paramenter.name)=\(value)"
+            }).joined(separator: "&")
+            string += argumentString
+        }
+        
+        return string
+    }
+    /// Computed & constructed API url
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    /// Desired http method
+    public let httpMethod = "GET"
+    
+    /// Construct request
+    /// - Parameters:
+    ///   - endpoint: Taget endpoint
+    ///   - pathComponents: Collection of Path components
+    ///   - queryParameters: Collections of query parameters
+    public init(endpoint: RMEndpoint, pathComponents: Set<String> = [], queryParameters: [URLQueryItem] = []) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
--- RickAndMorty_UIKit/Models/Episode.swift
+++ RickAndMorty_UIKit/Models/Episode.swift
@@ -1,8 +1,18 @@
 //
-//  RMEpisode.swift
+//  Episode.swift
 //  RickAndMorty_UIKit
 //
 //  Created by Sonic on 22/4/23.
 //
 
 import Foundation
+
+struct Episode: Codable {
+    
+    let id: Int
+    let name: String
+    let air_date: String
+    let episode: String
+    let characters: [String]
+    let created: String
+}
